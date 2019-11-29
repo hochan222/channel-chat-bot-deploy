2fee108a853997e2ce2ef207081d4ae6f8ce9586
hochan222 <201502766@hufs.ac.kr>
2019-11-29 21:24:43 +0900

    daw

--- app.py
+++ app.py
@@ -7,68 +7,108 @@ app = Flask(__name__)
 
 def skill():
     print(request.json)
-    data = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "아래 노란 버튼이 퀵 리플라이스"
+    if str(request.json['userRequest']['utterance']) == "퀵플라이1 응답메세지":
+        data ={
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        # "simpleImage": {
+                        #     "imageUrl": "https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
+                        #     "altText": "HUFS 로고"
+                        # }
+                        "basicCard": {
+                            "title": "카드의 제목",
+                            "description": "상세 설명",
+                            "thumbnail": {
+                                "imageUrl":"https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
+                                "link": {
+                                    "mobile": "https://naver.com",
+                                    "android": "https:google.com"
+                                }
+                            },
+                            "buttons": [
+                                {
+                                    # "label": "첫번째 버튼",
+                                    # "action": "webLink",
+                                    # "webLinkUrl": "https://portal.azure.com"
+                                    "label": "010-2500-1925에 전화걸기",
+                                    "action": "phone",
+                                    "phoneNumber": "010-2054-3247"
+                                },
+                                {
+                                    "label": "공유하기",
+                                    "action": "share"
+                                }
+                            ] 
+                        }
                     }
-                }
-            ],
-            "quickReplies": [
-                {
-                    "label": "퀵플라이1",
-                    "action": "message",
-                    "messageText": "퀵플라이1 응답메세지"
-                },
-                {
-                    "label": "퀵플라이2",
-                    "action": "message",
-                    "messageText": "퀵플라이2 응답메세지"
-                },
-                {
-                    "label": "퀵플라이3",
-                    "action": "message",
-                    "messageText": "퀵플라이3 응답메세지"
-                }
-            ]
-            # "outputs": [
-            #     {
-            #         # "simpleImage": {
-            #         #     "imageUrl": "https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
-            #         #     "altText": "HUFS 로고"
-            #         # }
-            #         "basicCard": {
-            #             "title": "카드의 제목",
-            #             "description": "상세 설명",
-            #             "thumbnail": {
-            #                 "imageUrl":"https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
-            #                 "link": {
-            #                     "mobile": "https://naver.com",
-            #                     "android": "https:google.com"
-            #                 }
-            #             },
-            #             "buttons": [
-            #                 {
-            #                     # "label": "첫번째 버튼",
-            #                     # "action": "webLink",
-            #                     # "webLinkUrl": "https://portal.azure.com"
-            #                     "label": "010-2500-1925에 전화걸기",
-            #                     "action": "phone",
-            #                     "phoneNumber": "010-2054-3247"
-            #                 },
-            #                 {
-            #                     "label": "공유하기",
-            #                     "action": "share"
-            #                 }
-            #             ] 
-            #         }
-            #     }
-            # ]
+                ]
+            }
         }
-    }
+    else:
+        data = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "사용자 Key: "+ str(request.json['userRequest']['utterance'])
+                        }
+                    }
+                ],
+                "quickReplies": [
+                    {
+                        "label": "퀵플라이1",
+                        "action": "message",
+                        "messageText": "퀵플라이1 응답메세지"
+                    },
+                    {
+                        "label": "퀵플라이2",
+                        "action": "message",
+                        "messageText": "퀵플라이2 응답메세지"
+                    },
+                    {
+                        "label": "퀵플라이3",
+                        "action": "message",
+                        "messageText": "퀵플라이3 응답메세지"
+                    }
+                ]
+                # "outputs": [
+                #     {
+                #         # "simpleImage": {
+                #         #     "imageUrl": "https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
+                #         #     "altText": "HUFS 로고"
+                #         # }
+                #         "basicCard": {
+                #             "title": "카드의 제목",
+                #             "description": "상세 설명",
+                #             "thumbnail": {
+                #                 "imageUrl":"https://cf.festa.io/img/2019-11-14/791369de-e762-4cc1-a341-68ce8c4a467f.png",
+                #                 "link": {
+                #                     "mobile": "https://naver.com",
+                #                     "android": "https:google.com"
+                #                 }
+                #             },
+                #             "buttons": [
+                #                 {
+                #                     # "label": "첫번째 버튼",
+                #                     # "action": "webLink",
+                #                     # "webLinkUrl": "https://portal.azure.com"
+                #                     "label": "010-2500-1925에 전화걸기",
+                #                     "action": "phone",
+                #                     "phoneNumber": "010-2054-3247"
+                #                 },
+                #                 {
+                #                     "label": "공유하기",
+                #                     "action": "share"
+                #                 }
+                #             ] 
+                #         }
+                #     }
+                # ]
+            }
+        }
     return jsonify(data)
 
 def test():
